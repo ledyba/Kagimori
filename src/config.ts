4e6ded12f60d2772064531c887736de4e2ceff32
Kaede Fujisaki <kaede@hexe.net>
2020-09-05 19:09:54 +0900

    import対応

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,57 @@
 import { browser } from 'webextension-polyfill-ts'
 import Key from './Key'
-import { loadKeys, clearKeys } from './Repo';
+import { loadKeys, clearKeys, saveKeys } from './Repo';
 
-function doImport() {
+let messgeTimeout: NodeJS.Timeout | null = null;
+function showMessage(message: string, success: boolean) {
+  let messageBox = document.getElementById('message-box')!;
+	if (success) {
+		messageBox.className = 'visible success';
+	} else {
+		messageBox.className = 'visible error';
+	}
+  messageBox.innerText = message;
 
+  if(messgeTimeout) {
+    clearTimeout(messgeTimeout);
+  }
+
+	messgeTimeout = setTimeout(function() {
+    messageBox.className = '';
+	}, 3 * 1000);
+}
+
+function doImport(event: Event) {
+  const file = (event.target as HTMLInputElement)?.files;
+  if (!file) {
+    return;
+  }
+  let reader = new FileReader();
+  
+  reader.onload = function(e) {
+    let data: [Key];
+    let result = reader.result;
+    try {
+      data = JSON.parse(result as string);
+    } catch(e) {
+      showMessage('Failed to parse JSON data, invalid JSON:\n' + (e.message||'???'), false);
+      return;
+    }
+    async function handler() {
+      await saveKeys(data);
+      return data;
+    }
+    function onLoaded() {
+      showMessage('Imported!', true);
+    }
+    handler().then(onLoaded).catch();
+  };
+
+  try {
+    reader.readAsText(file[0], 'utf-8');
+  } catch(e) {
+    showMessage('Failed to read import file:\n' + (e.message||'???'), false);
+  }
 }
 
 function doExport() {
@@ -16,14 +64,22 @@ function doExport() {
     link.href = url;
     link.click();
   }
-  handler().catch(alert);
+  handler().then(() => {
+    showMessage('Exported!', true);
+  }).catch((err: any) => {
+    showMessage('Failed to export:\n' + err, false);
+  });
 }
 
 function doClear() {
   async function handler() {
-    const keys = await clearKeys();
+    await clearKeys();
   }
-  handler().catch(alert);
+  handler().then(() => {
+    showMessage('Cleard!', true);
+  }).catch((err: any) => {
+    showMessage('Failed to clear:\n' + err, false);
+  });
 }
 
 function main() {
@@ -44,12 +100,13 @@ function main() {
     ev.preventDefault();
     return false;
   });
-  importButton.addEventListener('click', (ev: MouseEvent) => {
+
+  const importInput: HTMLInputElement = document.getElementById('import-input')! as HTMLInputElement;
+  importInput.addEventListener('change', (ev: Event) => {
     ev.preventDefault();
-    doImport();
+    doImport(ev);
     return false;
   });
-
 
   const clearKeys: HTMLDivElement = document.getElementById('clear-button')! as HTMLDivElement;
   clearKeys.addEventListener('dragstart', (ev: MouseEvent) => {
