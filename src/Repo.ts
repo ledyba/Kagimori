d883de17078d1a8165bb04379e17df4a211a2089
Kaede Fujisaki <kaede@hexe.net>
2020-09-08 00:45:25 +0900

    Fix icon path

--- src/Repo.ts
+++ src/Repo.ts
@@ -1,7 +1,8 @@
 import { browser } from 'webextension-polyfill-ts'
 import {Key} from './Key'
 
-const storage =  browser.storage.sync;
+//const storage =  browser.storage.sync;
+const storage =  browser.storage.local;
 
 export async function loadKeys(): Promise<Array<Key>> {
   const v = await storage.get('keys');
